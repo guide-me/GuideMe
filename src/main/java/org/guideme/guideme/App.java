c71b65016cb00f9f5d156054c434e561bcf7eb41
Takenaga <takenaga.tanabe@gmail.com>
2013-07-27 17:40:40 +0200

    Sample html-document and simple parsing.

--- src/main/java/org/guideme/guideme/App.java
+++ src/main/java/org/guideme/guideme/App.java
@@ -1,8 +1,13 @@
 package org.guideme.guideme;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.eclipse.swt.widgets.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 /**
  * Hello world!
@@ -17,8 +22,21 @@ public class App
         System.out.println("Hello World!");
         log.info("Hello Logging World!");
         
+        String title = "Hello World!";
+        
+        try {
+        	File guideFile = new File("data/sample-1.html");
+			Document guideDocument = Jsoup.parse(guideFile, "UTF-8", "");
+			
+			title = guideDocument.select("head title").text();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
         Display display = new Display();
         Shell shell = new Shell(display);
+        shell.setText(title);
         shell.open();
         while (!shell.isDisposed()) {
     		if (!display.readAndDispatch()) {
