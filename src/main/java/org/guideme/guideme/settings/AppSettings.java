9a975e57fb4a4d9e10dcd7f62b1c5ee23764e699
philormand <philormand@hotmail.com>
2013-08-28 20:51:44 +0100

    Test cases for AppSettings and Common Settings

---
+++ src/main/java/org/guideme/guideme/settings/AppSettings.java
@@ -0,0 +1,112 @@
+package org.guideme.guideme.settings;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AppSettings {
+	private Logger logger = LogManager.getLogger();
+	private int FontSize = 20;
+	private int HtmlFontSize = 20;
+	private boolean Debug = false;
+	private String DataDirectory = "%userprofile%\\My Pictures";
+	private int[] sash1Weights = new int[2];
+	private int[] sash2Weights = new int[2];
+	private Properties appSettings = new Properties();
+	private String settingsLocation = "settings.properties";
+
+	public AppSettings() {
+		super();
+		try {
+			try {
+				appSettings.loadFromXML(new FileInputStream(settingsLocation));
+			}
+			catch (IOException ex) {
+				//failed to load file so just carry on
+				logger.error(ex.getLocalizedMessage(), ex);
+			}
+			FontSize = Integer.parseInt(appSettings.getProperty("FontSize", "20"));
+			HtmlFontSize = Integer.parseInt(appSettings.getProperty("HtmlFontSize", "20"));
+			Debug = Boolean.parseBoolean(appSettings.getProperty("Debug", "false"));
+			DataDirectory = appSettings.getProperty("DataDirectory", "%userprofile%\\My Pictures");
+			sash1Weights[0] = Integer.parseInt(appSettings.getProperty("sash1Weights0", "350"));
+			sash1Weights[1] = Integer.parseInt(appSettings.getProperty("sash1Weights1", "350"));
+			sash2Weights[0] = Integer.parseInt(appSettings.getProperty("sash2Weights0", "800"));
+			sash2Weights[1] = Integer.parseInt(appSettings.getProperty("sash2Weights1", "200"));
+		}
+		catch (Exception ex) {
+			logger.error(ex.getLocalizedMessage(), ex);
+		}
+		saveSettings();
+	}
+
+	public int getFontSize() {
+		return FontSize;
+	}
+
+	public void setFontSize(int fontSize) {
+		FontSize = fontSize;
+	}
+
+	public boolean getDebug() {
+		return Debug;
+	}
+
+	public void setDebug(boolean debug) {
+		Debug = debug;
+	}
+
+	public String getDataDirectory() {
+		return DataDirectory;
+	}
+
+	public void setDataDirectory(String dataDirectory) {
+		DataDirectory = dataDirectory;
+	}
+
+	public int[] getSash1Weights() {
+		return sash1Weights;
+	}
+
+	public void setSash1Weights(int[] sash1Weights) {
+		this.sash1Weights = sash1Weights;
+	}
+
+	public int[] getSash2Weights() {
+		return sash2Weights;
+	}
+
+	public void setSash2Weights(int[] sash2Weights) {
+		this.sash2Weights = sash2Weights;
+	}
+
+	public void saveSettings() {
+		try {
+			appSettings.setProperty("FontSize", String.valueOf(FontSize));
+			appSettings.setProperty("HtmlFontSize", String.valueOf(HtmlFontSize));
+			appSettings.setProperty("Debug", String.valueOf(Debug));
+			appSettings.setProperty("DataDirectory", DataDirectory);
+			appSettings.setProperty("sash1Weights0", String.valueOf(sash1Weights[0]));
+			appSettings.setProperty("sash1Weights1", String.valueOf(sash1Weights[1]));
+			appSettings.setProperty("sash2Weights0", String.valueOf(sash2Weights[0]));
+			appSettings.setProperty("sash2Weights1", String.valueOf(sash2Weights[1]));
+			appSettings.storeToXML(new FileOutputStream(settingsLocation), null);
+		}
+		catch (Exception e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	public int getHtmlFontSize() {
+		return HtmlFontSize;
+	}
+
+	public void setHtmlFontSize(int htmlFontSize) {
+		HtmlFontSize = htmlFontSize;
+	}
+
+}
---
+++ src/main/java/org/guideme/guideme/settings/PresSettings.java
@@ -0,0 +1,178 @@
+package org.guideme.guideme.settings;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class PresSettings {
+	//State information for xml file, stored in a .state file in xml format
+	private String page = "start"; //current page
+	private String flags = ""; //current flags
+	private String html = ""; //used to pass back replacement html from javascript 
+	private String filename; //name of file to store persistent state
+	private HashMap<String, String> scriptVariables = new HashMap<String, String>(); //variables used by javascript
+	private Logger logger = LogManager.getLogger();
+
+	public PresSettings(String PresName) {
+		super();
+		AppSettings appSettings = new AppSettings();
+		filename = appSettings.getDataDirectory() + "\\" + PresName + ".state";
+		try {
+			//if a state file already exists use it 
+			File xmlFile = new File(filename);
+
+			if (xmlFile.exists()) {
+				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+				Document doc = docBuilder.parse(xmlFile);
+				Element rootElement = doc.getDocumentElement();
+				rootElement.normalize();
+
+				Element elPage = ComonFunctions.getElement("//Page", rootElement);
+				if (elPage != null) {
+					setPage(elPage.getTextContent());
+				}
+
+				Element elFlags = ComonFunctions.getElement("//Flags", rootElement);
+				if (elFlags != null) {
+					setFlags(elFlags.getTextContent());
+				}
+				
+				Element elScriptVariables = ComonFunctions.getElement("//scriptVariables", rootElement);
+				if (elScriptVariables != null) {
+					NodeList nodeList = elScriptVariables.getElementsByTagName("Var");
+					String strName;
+					String strValue;
+					for (int i = 0; i < nodeList.getLength(); i++) {
+						Node currentNode = nodeList.item(i);
+						if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+							Element elVar = (Element) currentNode;
+							strName = elVar.getAttribute("id");
+							strValue = elVar.getAttribute("value");
+							scriptVariables.put(strName, strValue);
+						}
+					}
+				}
+
+			}
+
+		} catch (ParserConfigurationException pce) {
+			logger.error(pce.getLocalizedMessage(), pce);
+		} catch (SAXException e) {
+			logger.error(e.getLocalizedMessage(),e);
+		} catch (IOException e) {
+			logger.error(e.getLocalizedMessage(),e);
+		}
+		saveSettings();
+	}
+
+	public String getPage() {
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+
+	public String getFlags() {
+		return flags;
+	}
+
+	public void setFlags(String flags) {
+		this.flags = flags;
+	}
+
+	public HashMap<String, String> getScriptVariables() {
+		return scriptVariables;
+	}
+
+	public void setScriptVariables(HashMap<String, String> scriptVariables) {
+		this.scriptVariables = scriptVariables;
+	}
+
+	public String getHtml() {
+		return html;
+	}
+
+	public void setHtml(String html) {
+		this.html = html;
+	}
+	public void saveSettings(){
+	    try {
+			File xmlFile = new File(filename);
+			Element rootElement;
+			Document doc;
+
+			// if the file exists then use the current one, otherwise create a new one.
+			// if nodes do not exist it will add them
+			if (xmlFile.exists())
+			{
+				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+				doc = docBuilder.parse(xmlFile);
+				rootElement = doc.getDocumentElement();
+				rootElement.normalize();
+			} else {
+				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+				doc = docBuilder.newDocument();
+				rootElement = doc.createElement("SETTINGS");
+				doc.appendChild(rootElement);
+			}
+
+		    Element elPage = ComonFunctions.getOrAddElement("//Page", "Page", rootElement, doc);
+		    elPage.setTextContent(getPage());
+
+		    Element elFlags = ComonFunctions.getOrAddElement("//Flags", "Flags", rootElement, doc);
+		    elFlags.setTextContent(getFlags());
+
+		    Element elScriptVariables = ComonFunctions.getElement("//scriptVariables", rootElement);
+		    if (elScriptVariables != null) {
+		    	rootElement.removeChild(elScriptVariables);
+		    }
+		    elScriptVariables = ComonFunctions.addElement("scriptVariables", rootElement, doc);
+		    
+		    Iterator<String> it = scriptVariables.keySet().iterator();
+		    Element elVar;
+		    while (it.hasNext()) {
+		    	String key = it.next();
+		    	elVar = ComonFunctions.addElement("Var", elScriptVariables, doc);
+		    	elVar.setAttribute("id", key);
+		    	Object value = scriptVariables.get(key);
+		    	String strValue = String.valueOf(value);
+		    	elVar.setAttribute("value", strValue);
+		    }		    
+
+			// write the content into xml file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(filename));
+			transformer.transform(source, result);
+
+		} catch (TransformerException ex) {
+			logger.error(ex.getLocalizedMessage(),ex);
+		} catch (Exception ex ) {
+			logger.error(ex.getLocalizedMessage(),ex);
+		}
+	}
+
+}
