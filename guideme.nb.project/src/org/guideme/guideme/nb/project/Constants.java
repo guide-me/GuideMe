16959697af4d0d9c35eca697ae6257618d667ea0
TakenagaT <takenaga.tanabe@gmail.com>
2014-07-27 14:02:58 +0200

    Basic support Guide Projects.

---
+++ guideme.nb.project/src/org/guideme/guideme/nb/project/Constants.java
@@ -0,0 +1,8 @@
+package org.guideme.guideme.nb.project;
+
+public class Constants {
+
+    public static final String GM_DIR = "_gm";
+    
+    public static final String GUIDE_FILE = "guide.xml";
+}
