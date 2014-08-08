add71218e3d944df81491eb571716d567652fa24
TakenagaT <takenaga.tanabe@gmail.com>
2014-08-08 11:47:33 +0200

    Added text, including unit tests.

--- guideme.milovana/src/org/guideme/guideme/milovana/FlashTeaseConverter.java
+++ guideme.milovana/src/org/guideme/guideme/milovana/FlashTeaseConverter.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.guideme.guideme.milovana.nyxparser.NyxScriptBaseListener;
 import org.guideme.guideme.milovana.nyxparser.NyxScriptLexer;
 import org.guideme.guideme.milovana.nyxparser.NyxScriptParser;
@@ -13,40 +14,39 @@ import org.guideme.guideme.model.Page;
 import org.openide.util.Exceptions;
 
 public class FlashTeaseConverter {
-    
+
     public Guide createGuide(String input) {
-        
+
         try {
             ANTLRInputStream antlrStream = new ANTLRInputStream(IOUtils.toInputStream(input, "UTF-8"));
             NyxScriptLexer lexer = new NyxScriptLexer(antlrStream);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             NyxScriptParser parser = new NyxScriptParser(tokens);
             ParseTree tree = parser.guide();
-            
+
             ParseTreeWalker walker = new ParseTreeWalker();
             NyxScriptGuideBuilder builder = new NyxScriptGuideBuilder();
 
             walker.walk(builder, tree);
-            
+
             return builder.getGuide();
-            
+
         } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
             return null;
         }
     }
-    
-    
+
     private class NyxScriptGuideBuilder extends NyxScriptBaseListener {
 
         private final Guide guide;
-        
+
         private Page currentPage;
-        
+
         public NyxScriptGuideBuilder() {
             guide = new Guide();
         }
-        
+
         public Guide getGuide() {
             return guide;
         }
@@ -54,10 +54,12 @@ public class FlashTeaseConverter {
         @Override
         public void enterPage(NyxScriptParser.PageContext ctx) {
             currentPage = new Page();
-            
+
             TerminalNode pageId = ctx.PAGE_ID();
             if (pageId != null) {
-                currentPage.setId(pageId.getText());
+                // if pageId still ends with a #, strip it.
+                String pageIdText =  StringUtils.stripEnd(pageId.getText().trim(), "#");
+                currentPage.setId(pageIdText);
             }
             super.enterPage(ctx);
         }
@@ -70,11 +72,20 @@ public class FlashTeaseConverter {
             }
             super.exitPage(ctx);
         }
-        
-        
-        
-        
-        
+
+        @Override
+        public void enterText(NyxScriptParser.TextContext ctx) {
+            if (currentPage != null) {
+                TerminalNode quotedString = ctx.QUOTED_STRING();
+                if (quotedString != null) {
+                    // Strip surrounding quotes.
+                    String textString = StringUtils.strip(quotedString.getText().trim(), "\"'’");
+                    currentPage.setText(textString);
+                }
+            }
+            super.enterText(ctx);
+        }
+
     }
 
 }
--- guideme.milovana/test/unit/src/org/guideme/guideme/milovana/tests/FlashTeaseConverterTest.java
+++ guideme.milovana/test/unit/src/org/guideme/guideme/milovana/tests/FlashTeaseConverterTest.java
@@ -30,8 +30,33 @@ public class FlashTeaseConverterTest {
     }
     
     @Test
-    public void absoluteTruth() {
+    public void emptyPageWithId() {
         Guide guide = sut.createGuide("start#page()");
-        assertEquals("start#", guide.getPages().get(0).getId());
+        assertEquals("start", guide.getPages().get(0).getId());
+    }
+    
+    @Test
+    public void twoPages() {
+        Guide guide = sut.createGuide("start#page()\np2#page()\n");
+        assertEquals(2, guide.getPages().size());
+        assertEquals("p2", guide.getPages().get(1).getId());
+    }
+    
+    @Test
+    public void simpleText() {
+        Guide guide = sut.createGuide("start#page(text:'hello')");
+        assertEquals("hello", guide.getPages().get(0).getText());
+    }
+    
+    @Test
+    public void textWithMarkup() {
+        Guide guide = sut.createGuide("start#page(text:'<p>hello</p>')");
+        assertEquals("<p>hello</p>", guide.getPages().get(0).getText());
+    }
+    
+    @Test
+    public void textWithQuotes() {
+        Guide guide = sut.createGuide("start#page(text:'hello \"stranger\".')");
+        assertEquals("hello \"stranger\".", guide.getPages().get(0).getText());
     }
 }
